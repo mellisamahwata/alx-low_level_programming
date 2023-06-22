304760005073604667a7ea893a3f8c5df905393e
mellisamahwata <mellisamahwata@gmail.com>
2023-06-22 04:35:58 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-  * times_table - prints the 9 times table, starting with 0
-  */
+ * times_table - prints the 9 times table, starting with 0
+ */
 void times_table(void)
 {
 int i, j, k;
@@ -35,6 +35,7 @@ _putchar((k % 10) + '0');
 }
 }
 }
+
 _putchar('\n');
 }
 }
