aa384eb9c74b86cebcdc2fe5b188f377cce3b970
mellisamahwata <mellisamahwata@gmail.com>
2023-06-22 04:43:38 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,26 +8,26 @@
   */
 void print_to_98(int n)
 {
-    int i;
+int i;
 
-    if (n <= 98)
-    {
-        for (i = n; i <= 98; i++)
-        {
-            if (i != 98)
-                printf("%d, ", i);
-            else
-                printf("%d\n", i);
-        }
-    }
-    else
-    {
-        for (i = n; i >= 98; i--)
-        {
-            if (i != 98)
-                printf("%d, ", i);
-            else
-                printf("%d\n", i);
-        }
-    }
+if (n <= 98)
+{
+for (i = n; i <= 98; i++)
+{
+if (i != 98)
+printf("%d, ", i);
+else
+printf("%d\n", i);
+}
+}
+else
+{
+for (i = n; i >= 98; i--)
+{
+if (i != 98)
+printf("%d, ", i);
+else
+printf("%d\n", i);
+}
+}
 }
