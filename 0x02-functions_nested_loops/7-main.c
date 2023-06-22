54a4821155e9c07341b915bd0f9a1b8222115789
mellisamahwata <mellisamahwata@gmail.com>
2023-06-22 01:36:35 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -9,9 +9,9 @@ int main(void)
 {
 int r;
 
-_print_last_digit(98);
-_print_last_digit(0);
-r = _print_last_digit(-1024);
+print_last_digit(98);
+print_last_digit(0);
+r = print_last_digit(-1024);
 _putchar('0' + r);
 _putchar('\n');
 return (0);
