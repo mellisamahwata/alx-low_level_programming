307e5453cba98a1c9c0e800f0e6e7eaf42dbcc42
mellisamahwata <mellisamahwata@gmail.com>
2023-06-23 01:43:39 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,22 +3,19 @@
 
 /**
  * Prints a triangle pattern using '#'.
- *
- *@param size The size of the triangle
- *
+ * @param size The size of the triangle
  * Prints a triangle pattern of the specified size using the '#' character.
  * If size is 0 or less, only a new line is printed.
- */
+*/
 void print_triangle(int size)
 {
+int i, j;
+
 if (size <= 0)
 {
 printf("\n");
 return;
 }
-
-int i, j;
-
 for (i = 0; i < size; i++)
 {
 for (j = 0; j <= i; j++)
