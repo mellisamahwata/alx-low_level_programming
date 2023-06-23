9122c9a5f9e97bb90fd5185936bafe0ad8fdc3d5
mellisamahwata <mellisamahwata@gmail.com>
2023-06-23 10:08:05 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,9 +3,10 @@
 
 /**
  * Prints a triangle pattern using '#'.
- * @param size The size of the triangle
- * Prints a triangle pattern of the specified size using the '#' character.
- * If size is 0 or less, only a new line is printed.
+ *
+ * @param size The size of the triangle (must be a positive integer)
+ *
+ * Prints a triangle pattern of the specified size using the '#' character.If size is 0 or less, only a new line is printed.
 */
 void print_triangle(int size)
 {
@@ -16,6 +17,7 @@ if (size <= 0)
 printf("\n");
 return;
 }
+
 for (i = 0; i < size; i++)
 {
 for (j = 0; j <= i; j++)
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,22 +1,41 @@
+#include "main.h"
 #include <stdio.h>
 
-int main(void) : Entry point of program. Returns exit status.
-{
-int i;
+/**
+  * main - Prints a Fizz Buzz program
+  * Return: Always 0 (success)
+  */
 
-for (i = 1; i <= 100; i++)
+int main(void)
+
 {
-if (i % 3 == 0 && i % 5 == 0)
-printf("FizzBuzz ");
-else if (i % 3 == 0)
-printf("Fizz ");
-else if (i % 5 == 0)
-printf("Buzz ");
-else
-printf("%d ", i);
+
+int x;
+
+for (x = 1; x <= 100; x++)
+{
+if ((x % 3 == 0) && (x % 5 == 0))
+{
+printf("FizzBuzz");
 }
-
+else if (x % 3 == 0)
+{
+printf("Fizz");
+}
+else if (x % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", x);
+}
+if (x != 100)
+{
+printf(" ");
+}
+}
 printf("\n");
-
 return (0);
+
 }
