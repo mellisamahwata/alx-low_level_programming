23fa45180c508af7822854b46e8ef9e8b3402419
mellisamahwata <mellisamahwata@gmail.com>
2023-06-29 07:10:20 -0700

    More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - Prints the elements of an array.
+ * @a: An array of integers.
+ * @n: The number of elements in the array.
+ *
+ * Function takes an array of integers and the number of elements in the
+ * array as parameters.
+ * It prints the elements of the array, separated by commas.
+ */
+void print_array(int *a, int n)
+{
+int i;
+
+i = 0;
+while (i < n)
+{
+if (i != 0)
+{
+printf(", ");
+}
+printf("%d", a[i]);
+i++;
+}
+printf("\n");
+}
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+
+print_array(a, sizeof(a) / sizeof(int));
+reverse_array(a, sizeof(a) / sizeof(int));
+print_array(a, sizeof(a) / sizeof(int));
+return (0);
+}
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - Converts all lowercase characters in a string to uppercase.
+ * @str: The string to convert.
+ *
+ * Return: A pointer to the converted string.
+ */
+char *string_toupper(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 32;
+}
+i++;
+}
+
+return (str);
+}
